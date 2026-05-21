7eddfeb42d23eda212ea09057100331d210f2faa
mahmoud.wahba.dev <mahmoud.wahba.dev@gmail.com>
2026-05-21 05:04:26 +0300

    update

---
+++ resources/views/landing_v1/pages/auth/register.blade.php
@@ -0,0 +1,259 @@
+@extends('landing_v1.layouts.app')
+
+@section('content')
+@php($landingImg = asset('assets/landing_v1/img/auth'))
+<main class="bg-[#F5F8F9] overflow-hidden">
+
+    <header class="m-16 rounded-20px bg-white px-9 py-15 shadow">
+        <div class="">
+            <div class="grid grid-cols-1 lg:grid-cols-2 lg:gap-20 gap-4">
+                <div class="">
+                    <div>
+                        <h1 class="font-semibold text-36px text-primary mb-2">ابدأ رحلتك المهنية الآن</h1>
+                        <p class="font-medium text-base text-primary mb-9">خطوة واحدة تفصلك عن الوصول لمكتبة واسعة من
+                            الخبرات الميدانية</p>
+
+                        <div>
+                            <nav class="tabs grid grid-cols-3 flex-nowrap h-20  space-x-1 rtl:space-x-reverse p-1 rounded-10px border mb-6 border-[#CCCCCC] h-16"
+                                aria-label="Tabs" role="tablist" aria-orientation="horizontal">
+                                <button type="button"
+                                    class="btn btn-text  active-tab:bg-primary h-full font-medium text-20px text-primary active-tab:text-white hover:text-primary active hover:bg-primary/20"
+                                    id="tabs-pill-icon-item-1" data-tab="#tabs-pill-icon-1"
+                                    aria-controls="tabs-pill-icon-1" role="tab" aria-selected="false">
+                                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <g clip-path="url(#clip0_362_6135)">
+                                            <path
+                                                d="M19.8488 3.2395L10.2534 0.0410395C10.0893 -0.0136798 9.9118 -0.0136798 9.74765 0.0410395L0.152281 3.2395C-0.00693691 3.29257 -0.145418 3.3944 -0.243544 3.53056C-0.341671 3.66672 -0.394467 3.8303 -0.394455 3.99813V11.9943C-0.394455 12.2063 -0.31021 12.4097 -0.160253 12.5597C-0.0102966 12.7096 0.193088 12.7939 0.405159 12.7939C0.61723 12.7939 0.820615 12.7096 0.970572 12.5597C1.12053 12.4097 1.20477 12.2063 1.20477 11.9943V5.1076L4.56215 6.22606C3.67014 7.66716 3.38649 9.40336 3.77347 11.0534C4.16045 12.7035 5.18644 14.1326 6.62616 15.0268C4.82703 15.7325 3.27178 17.0089 2.13433 18.754C2.07517 18.8419 2.03408 18.9407 2.01344 19.0447C1.99281 19.1486 1.99304 19.2556 2.01412 19.3595C2.03521 19.4633 2.07672 19.562 2.13626 19.6496C2.19579 19.7373 2.27216 19.8122 2.36092 19.8701C2.44968 19.928 2.54906 19.9677 2.65328 19.9868C2.75751 20.006 2.8645 20.0042 2.96804 19.9817C3.07158 19.9591 3.1696 19.9162 3.2564 19.8554C3.34321 19.7946 3.41706 19.7172 3.47368 19.6276C4.97995 17.3167 7.3588 15.9923 10.0005 15.9923C12.6423 15.9923 15.0211 17.3167 16.5274 19.6276C16.6447 19.8019 16.8258 19.923 17.0317 19.9648C17.2376 20.0066 17.4517 19.9657 17.6276 19.851C17.8036 19.7362 17.9273 19.5568 17.9721 19.3515C18.0168 19.1462 17.979 18.9316 17.8667 18.754C16.7293 17.0089 15.168 15.7325 13.3749 15.0268C14.8132 14.1326 15.8383 12.7045 16.2252 11.0556C16.6121 9.40675 16.3293 7.67174 15.4389 6.23105L19.8488 4.76176C20.008 4.70872 20.1466 4.6069 20.2447 4.47074C20.3429 4.33458 20.3957 4.17098 20.3957 4.00313C20.3957 3.83527 20.3429 3.67168 20.2447 3.53552C20.1466 3.39936 20.008 3.29754 19.8488 3.24449V3.2395ZM14.7982 9.59543C14.7984 10.3539 14.6188 11.1017 14.2741 11.7773C13.9294 12.4529 13.4294 13.0372 12.8151 13.4821C12.2008 13.9271 11.4898 14.2201 10.7404 14.3371C9.99101 14.4541 9.22454 14.3917 8.5039 14.1551C7.78326 13.9185 7.12899 13.5144 6.59476 12.9759C6.06054 12.4375 5.66159 11.7801 5.43064 11.0576C5.1997 10.3351 5.14334 9.56818 5.2662 8.8197C5.38905 8.07123 5.68762 7.36255 6.13739 6.7518L9.74765 7.95122C9.9118 8.00594 10.0893 8.00594 10.2534 7.95122L13.8637 6.7518C14.4711 7.57543 14.7986 8.57203 14.7982 9.59543ZM10.0005 6.35399L2.93394 3.99813L10.0005 1.64227L17.0671 3.99813L10.0005 6.35399Z"
+                                                fill="#3D455D" />
+                                        </g>
+                                        <defs>
+                                            <clipPath id="clip0_362_6135">
+                                                <rect width="20" height="20" fill="white" />
+                                            </clipPath>
+                                        </defs>
+                                    </svg>
+
+
+
+                                    <span class="hidden sm:inline">طالب</span>
+                                </button>
+                                <button type="button"
+                                    class="btn btn-text active-tab:bg-primary h-full font-medium text-20px text-primary active-tab:text-white hover:text-primary  hover:bg-primary/20"
+                                    id="tabs-pill-icon-item-2" data-tab="#tabs-pill-icon-2"
+                                    aria-controls="tabs-pill-icon-2" role="tab" aria-selected="false">
+                                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <g clip-path="url(#clip0_362_6140)">
+                                            <path
+                                                d="M0 -0.140625H20V4.7316H17.7778V2.0816H2.22222V12.0816H7.03667V14.3038H0V-0.140625Z"
+                                                fill="#3D455D" />
+                                            <path
+                                                d="M10.7593 7.95908C11.7304 8.98686 12.5582 9.86241 14.4438 9.86241H16.666C17.2554 9.86241 17.8206 10.0965 18.2373 10.5133C18.6541 10.93 18.8882 11.4953 18.8882 12.0846V13.1957C18.8882 13.5858 18.7855 13.969 18.5905 14.3068C18.3954 14.6446 18.1149 14.9252 17.7771 15.1202V20.9735H14.4438V12.0846C11.536 12.0846 10.0482 10.4735 9.06598 9.41241C8.93281 9.26613 8.79686 9.1224 8.6582 8.9813L10.2293 7.41019C10.4138 7.59538 10.5904 7.77834 10.7593 7.95908ZM17.7771 7.08464C17.7771 7.52666 17.6015 7.95059 17.2889 8.26315C16.9764 8.57571 16.5525 8.7513 16.1104 8.7513C15.6684 8.7513 15.2445 8.57571 14.9319 8.26315C14.6194 7.95059 14.4438 7.52666 14.4438 7.08464C14.4438 6.64261 14.6194 6.21868 14.9319 5.90612C15.2445 5.59356 15.6684 5.41797 16.1104 5.41797C16.5525 5.41797 16.9764 5.59356 17.2889 5.90612C17.6015 6.21868 17.7771 6.64261 17.7771 7.08464Z"
+                                                fill="#3D455D" />
+                                        </g>
+                                        <defs>
+                                            <clipPath id="clip0_362_6140">
+                                                <rect width="20" height="20" fill="white" />
+                                            </clipPath>
+                                        </defs>
+                                    </svg>
+
+
+
+                                    <span class="hidden sm:inline">متدرب</span>
+                                </button>
+
+
+                                <button type="button"
+                                    class="btn btn-text active-tab:bg-primary h-full font-medium text-20px text-primary active-tab:text-white hover:text-primary  hover:bg-primary/20"
+                                    id="tabs-pill-icon-item-3" data-tab="#tabs-pill-icon-3"
+                                    aria-controls="tabs-pill-icon-3" role="tab" aria-selected="false">
+                                    <svg width="17" height="19" viewBox="0 0 17 19" fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="M16 17.8106H1M3.5 9.47725V14.4773M8.5 9.47725V14.4773M13.5 9.47725V14.4773M1 5.31059V6.14392H16V5.31059L8.5 1.14392L1 5.31059Z"
+                                            stroke="#3D455D" stroke-width="2" stroke-linecap="square" />
+                                    </svg>
+
+
+
+                                    <span class="hidden sm:inline">مؤسسة</span>
+                                </button>
+                            </nav>
+
+
+                            <div class="mt-3">
+                                <div id="tabs-pill-icon-1" role="tabpanel" aria-labelledby="tabs-pill-icon-item-1">
+                                    <form class="needs-validation peer " novalidate>
+
+                                        <div class="mb-8">
+                                            <label class="label label-text font-medium text-24px text-primary mb-4"
+                                                for="contactEmail">الاسم كامل</label>
+                                            <input id="contactEmail" name="email" type="email" placeholder="الاسم كامل"
+                                                class="input bg-f7 h-16 rounded-7px w-full text-primary text-start"
+                                                autocomplete="email" required />
+                                            <span class="error-message text-14px text-secondary">يرجى إدخال اسم
+                                                صحيح.</span>
+                                            <span class="success-message text-14px text-primary/70">تم بنجاح.</span>
+                                        </div>
+                                        <!-- Email and password -->
+                                        <div class="mb-8">
+                                            <label class="label label-text font-medium text-24px text-primary mb-4"
+                                                for="contactEmail">البريد الالكتروني</label>
+                                            <input id="contactEmail" name="email" type="email"
+                                                placeholder="name@example.com"
+                                                class="input bg-f7 h-16 rounded-7px w-full text-primary text-start"
+                                                autocomplete="email" required />
+                                            <span class="error-message text-14px text-secondary">يرجى إدخال بريد
+                                                صحيح.</span>
+                                            <span class="success-message text-14px text-primary/70">تم بنجاح.</span>
+                                        </div>
+
+
+                                        <div class="mb-8">
+                                            <label class="label label-text font-medium text-24px text-primary mb-4"
+                                                for="contactPhone">الهاتف</label>
+                                            <input id="contactPhone" name="phone" type="tel" placeholder="05xxxxxxxx"
+                                                class="input bg-f7 h-16 rounded-7px w-full text-primary text-start"
+                                                dir="{{ app()->getLocale() === 'ar' ? 'rtl' : 'ltr' }}"
+                                                autocomplete="tel" required />
+                                            <span class="error-message text-14px text-secondary">يرجى إدخال رقم
+                                                الهاتف.</span>
+                                            <span class="success-message text-14px text-primary/70">تم بنجاح.</span>
+                                        </div>
+
+                                        <div class="mb-8">
+                                            <label class="label label-text font-medium text-24px text-primary mb-4"
+                                                for="contactName">كلمة المرور</label>
+                                            <input id="contactName" name="name" type="text" placeholder="كلمة المرور"
+                                                class="input bg-f7 h-16 rounded-7px w-full text-primary text-start"
+                                                autocomplete="name" required />
+                                            <span class="error-message text-14px text-secondary">يرجى إدخال
+                                                كلمة المرور.</span>
+                                            <span class="success-message text-14px text-primary/70">تم بنجاح.</span>
+                                        </div>
+
+
+
+
+
+
+                                        <!-- Submit button -->
+                                        <div class="mt-4">
+                                            <button type="submit" name="submitButton"
+                                                class="btn btn-gold btn-block h-16 rounded-8px font-semibold  text-24px">تسجيل
+                                                الدخول</button>
+                                        </div>
+                                    </form>
+
+
+                                </div>
+                                <div id="tabs-pill-icon-2" class="hidden" role="tabpanel"
+                                    aria-labelledby="tabs-pill-icon-item-2">
+
+
+
+
+                                    <div class="mb-8">
+                                        <label class="label label-text font-medium text-24px text-primary mb-4"
+                                            for="contactPhone">الجوال</label>
+                                        <input id="contactPhone" name="phone" type="tel" placeholder="05xxxxxxxx"
+                                            class="input bg-f7 h-16 rounded-7px w-full text-primary text-start"
+                                            dir="{{ app()->getLocale() === 'ar' ? 'rtl' : 'ltr' }}" autocomplete="tel"
+                                            required />
+                                        <span class="error-message text-14px text-secondary">يرجى إدخال رقم
+                                            الجوال.</span>
+                                        <span class="success-message text-14px text-primary/70">تم بنجاح.</span>
+                                    </div>
+
+                                    <div class="mb-8">
+                                        <label class="label label-text font-medium text-24px text-primary mb-4"
+                                            for="contactName">كلمة المرور</label>
+                                        <input id="contactName" name="name" type="text" placeholder="كلمة المرور"
+                                            class="input bg-f7 h-16 rounded-7px w-full text-primary text-start"
+                                            autocomplete="name" required />
+                                        <span class="error-message text-14px text-secondary">يرجى إدخال
+                                            كلمة المرور.</span>
+                                        <span class="success-message text-14px text-primary/70">تم بنجاح.</span>
+                                    </div>
+                                    <!-- Submit button -->
+                                    <div class="mt-4">
+                                        <button type="submit" name="submitButton"
+                                            class="btn btn-gold btn-block h-16 rounded-8px font-semibold  text-24px">تسجيل
+                                            الدخول</button>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div class="mt-7">
+                                <p class="font-medium text-20px text-[#979797] center">
+                                    ليس لديك حساب ؟
+                                    <a class="link link-animated font-semibold text-22px text-primary px-2"
+                                        href="/landing-v1/register">انشاء حساب</a>
+                                </p>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="">
+                    <div id="indicators "
+                        data-carousel='{ "loadingClasses": "opacity-0", "dotsItemClasses": "carousel-dot", "isRTL": true ,"isInfiniteLoop": true, "isAutoPlay": true, "isDraggable": true }'
+                        class="relative w-full h-full">
+                        <div class="carousel h-full">
+                            <div class="carousel-body h-full opacity-0">
+                                <!-- Slide 1 -->
+                                <div class="carousel-slide">
+                                    <div class="bg-primary rounded-20px py-11 px-7 h-full">
+                                        <div class="mb-20 h-[60%] center">
+                                            <img class="h-full" src="{{ $landingImg }}/register-slide.webp"
+                                                alt="Slide 1">
+                                        </div>
+                                        <p class="font-bold text-32px text-white text-center">
+                                            شهاداتك المعتمدة.. بانتظارك دائمًا
+                                        </p>
+                                    </div>
+
+                                </div>
+                                <!-- Slide 2 -->
+                                <div class="carousel-slide">
+                                    <div class="bg-primary rounded-20px py-11 px-7 h-full">
+                                        <div class="mb-20 h-[60%] center">
+                                            <img class="h-full" src="{{ $landingImg }}/register-slide.webp"
+                                                alt="Slide 1">
+                                        </div>
+                                        <p class="font-bold text-32px text-white text-center">
+                                            شهاداتك المعتمدة.. بانتظارك دائمًا
+                                        </p>
+                                    </div>
+
+                                </div>
+                                <div class="carousel-slide">
+                                    <div class="bg-primary rounded-20px py-11 px-7 h-full">
+                                        <div class="mb-20 h-[60%] center">
+                                            <img class="h-full" src="{{ $landingImg }}/register-slide.webp"
+                                                alt="Slide 1">
+                                        </div>
+                                        <p class="font-bold text-32px text-white text-center">
+                                            شهاداتك المعتمدة.. بانتظارك دائمًا
+                                        </p>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="carousel-pagination absolute bottom-3 end-0 start-0 flex justify-center gap-3">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+
+</main>
+@endsection
+@push('scripts')
+@endpush
